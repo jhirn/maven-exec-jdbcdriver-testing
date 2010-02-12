c8ae259436df5169565885ecb8f108ea620b3006
Joe Hirn <Joseph.Hirn@gmail.com>
2010-02-12 08:38:46 -0600

    updaed main to call printLoadedDrivers twice showing the problem exists within the same execution

--- src/main/java/com/hirn/DbUnitRunner.java
+++ src/main/java/com/hirn/DbUnitRunner.java
@@ -8,11 +8,16 @@ import java.util.Enumeration;
 public class DbUnitRunner {
 
   public static void main(final String[] args) {
-    System.out.println("\nDrivers Loaded\n\n");
+    printLoadedDrivers();
+    printLoadedDrivers();
+  }
+
+	private static void printLoadedDrivers() {
+		System.out.println("\nDrivers Loaded\n\n");
     for (Enumeration e = DriverManager.getDrivers();e.hasMoreElements();){
       Driver d = (Driver) e.nextElement();
       System.out.println(d);
     }
     System.out.println("\n\n");
-  }
+	}
 }
